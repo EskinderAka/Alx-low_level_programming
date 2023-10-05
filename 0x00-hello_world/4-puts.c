c336326284e26a02884b76542f24bc87217fe57b
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-05 03:18:02 -0700

    C Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main Entry point 
+ * Description: This C file calles puts function and recive value returned by puts after it excution.
+ * Return: The main function always returns 0 to show it success
+ **/
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
+
