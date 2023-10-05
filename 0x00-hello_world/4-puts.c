f06e0f47064283ec214687cd93489eb46ddd4b69
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-05 03:27:48 -0700

    C Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
 * execution.
 * Return: Always return 0 to indicate success
 */
-int main(void)
+int main (void)
 {
-puts("/"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
 
