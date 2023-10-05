935f052b29d6d45f6c25b17f9a6822ecb5d2f0f1
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-05 09:11:56 -0700

    C Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+* main- Entry point
+* @void: parameter
+* Return: 1 (success)
+**/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+return (1);
+}
+
