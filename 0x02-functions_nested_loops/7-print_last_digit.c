c01a46e9c58fbfde2c3b2d4e8e278e4bfa9aeaf4
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-10 04:11:23 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+/*
+ * File : 7-print_last_digit.c
+ * By   : Eskinder Aka
+ */
+
+#include "main.h"
+/**
+*print_last_digit - function that print last digit of a number
+*@n: number to be targeted
+*Return: returns 0
+*/
+int print_last_digit(int n)
+{
+int lastdigit = n % 10;
+if (lastdigit < 0)
+lastdigit *= -1;
+
+_putchar (lastdigit + '0');
+return (lastdigit);
+}
