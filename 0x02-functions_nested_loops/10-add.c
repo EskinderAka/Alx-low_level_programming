f964998d2a1c8a18d38fa37d2dd67e51f99037e5
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-10 04:59:01 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File : 10-add.c
+ * By   : Eskinder Aka
+ */
+
+#include "main.h"
+/**
+*add - sums up two numbers
+*@n:first integer
+*@m:second integer
+*Return: returns 0
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
