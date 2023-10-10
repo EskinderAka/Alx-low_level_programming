b532371e258e9197aca505deae664fe5f84b21a4
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-10 13:29:04 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,19 @@
-#include <stdio.h>
-
+#ifndef INV_TREE_H
+#define INV_TREE_H
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabt - print the lower-case alphabet on one line.
  */
-int main(void)
-{
-    return (0);
-}
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
+#endif
