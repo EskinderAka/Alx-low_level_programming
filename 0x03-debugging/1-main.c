5fac79e17b4f061ff49d35ad20b6a1a3517e3054
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-11 04:14:28 -0700

    1. Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,29 @@
+/*
+ * File : 1-main.c
+ * By   : Eskinder Aka
+ * Task : 1. Like, comment, subscribe
+ */
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
