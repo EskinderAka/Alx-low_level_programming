43411a7600b21f74d0174fd192ac105b0dff68be
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-12 15:18:58 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File : 2-main.c
+ * By   : ALXSE Prototype
+ * Task : 1.isdigit
+ */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */i
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+/*
+ * File : 2-mul.c
+ * By   : Eskinder Aka
+ * Task : 2. Collaboration is multiplication
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two integers.
+ * @a : variable
+ * @b : variable
+ * Return: 0 or 1 .
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
