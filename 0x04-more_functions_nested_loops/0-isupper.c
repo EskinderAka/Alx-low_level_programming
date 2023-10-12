92ad4a6cd2b8918887e3b25c56fb1fedf7a971df
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-12 13:23:26 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+/*
+ * File : 0-isupper.c
+ * By   : Protype
+ * Task : 0. isupper
+ */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
