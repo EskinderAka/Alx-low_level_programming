f06406f8936ccec84526cf4860371e8436c40a46
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-14 14:24:27 -0700

    Very good

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+/*
+ * File : 9-fizz_buzz.c
+ * by   : Eskinder Aka
+ * Task : 9. Fizz-Buzz
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+**
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
+ */
+
+ int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+		{
+			printf("%s", b);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s", bf);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", f);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", b);
+		}
+		else 
+		{
+			printf("\n");
+			return (0)
+		}
+	}
