9f92d22a8be3bd9c1ef82da201f38603cdd9f4c8
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-14 14:09:50 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,22 +14,22 @@
 
 void print_square(int size)
 {
-	int inc1, inc2;
+	int co, ro;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (inc1 = 0; inc1 < size; inc1++)
-		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-			{
-				_putchar('#');
-			}
-			_putchar('#');
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n')
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
