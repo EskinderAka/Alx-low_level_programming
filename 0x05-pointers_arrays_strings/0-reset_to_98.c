1b135f8f61e476e8aa5906ff9762e658f9d00144
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-16 10:00:02 -0700

    reset_to_98.c commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File : 0-reset_to_98.c
+ * By   : Eskinder Aka
+ * Task : 0. 98 Battery st.
+ */
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
