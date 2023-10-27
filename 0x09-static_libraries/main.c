be2f488cd731031d2d876ba7f1dd00d86ec43714
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-27 14:57:17 -0700

    task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,12 +2,13 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-_puts("\" At the end of the day,
-		my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+char *str;
+
+str = "\"At the end of the day, my goal was to be the best hacker \"\n\t- Kevin Mitnick";
+_puts(str);
 return (0);
 }
