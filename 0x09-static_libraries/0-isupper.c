f04f2d20d03682a37c5f6634c9b02d4afb447526
Eskinder Aka <eakalebrhane@gmail.com>
2023-10-27 14:44:30 -0700

    task 0 trial

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+/*
+ * File : 0-isupper
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character.
+ *@c : variable
+ * Return: 0 or 1 .
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
