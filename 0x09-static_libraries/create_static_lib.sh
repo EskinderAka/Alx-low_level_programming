eddfe004fa53ee7a0fe92b0d72e3890c3315fded
EskinderAka <eakalebrhane@gmail.com>
2023-10-28 00:10:16 -0700

    task 0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+# Compile each .c file into a corresponding .o file
+gcc -c *.c
+# Create the static library liball.a from the compiled .o files
 ar -rc liball.a *.o
+# Index the static library
 ranlib liball.a
+# Clean up the generated .o files
+rm *.o
